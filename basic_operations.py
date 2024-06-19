ff3f2668b38ea6ab944c6ed675f23d85af1b42a6
Baki78 <bakiget78@gmail.com>
2024-06-19 21:46:56 +0300

    first commit

---
+++ basic_operations.py
@@ -0,0 +1,14 @@
+
+#Declaring a variable number1 and number2
+number1 = 10
+number2 = 5
+
+#perform addition subtraction and product of the variables
+addition = number1 + number2
+subtraction = number2 - number1
+product = number1 * number2
+
+#printing all the results
+print("Addition of 10 and 5 is", addition)
+print("Subtraction of 10 and 5 is", subtraction)
+print("Multiplication of 10 and 5 is", product)
