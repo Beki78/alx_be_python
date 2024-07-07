1f424c1836b80d40c1eaaa43d65da0a78eb3defa
Baki78 <bakiget78@gmail.com>
2024-07-07 15:49:00 +0300

    seconf

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,6 +1,16 @@
-import datetime
+from datetime import datetime, timedelta
+
+
+
 def display_current_datetime():
-    current_date = datetime.datetime.now()
-    print  (current_date)
+    current_date = datetime.now()
+    print  (f"Current date and time:{current_date}")
 
 display_current_datetime()
+
+
+def calculate_future_date():
+        user = int(input("Enter a date: "))
+        future_date = timedelta(days=user)
+        print(future_date)
+calculate_future_date()
