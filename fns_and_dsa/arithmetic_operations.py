55152ca66c342bfef9f269e149b9469316027839
Baki78 <bakiget78@gmail.com>
2024-07-07 13:23:10 +0300

    using if

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,11 +1,35 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "divide":
-            return num1 / num2
-        case "multiply":
-            return num1 * num2
+    if operation == "add":
+        return num1 + num2
+    if operation == "subtract":
+        return num1 - num2
+    if operation == "divide":
+        return num1 / num2
+    if operation == "multiply":
+        return num1 * num2
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # match operation:
+    #     case "add":
+    #         return num1 + num2
+    #     case "subtract":
+    #         return num1 - num2
+    #     case "divide":
+    #         return num1 / num2
+    #     case "multiply":
+    #         return num1 * num2
         
