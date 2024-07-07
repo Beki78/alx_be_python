8b13a606013a1166eba3d68b66e68ce24db3a858
Baki78 <bakiget78@gmail.com>
2024-07-07 13:35:30 +0300

    last

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -4,7 +4,7 @@ def perform_operation(num1, num2, operation):
     elif operation == "subtract":
         return num1 - num2
     elif operation == "divide":
-        if num1 == 0:
+        if num2 == 0:
             return "error"
         else: 
             return num1 / num2
