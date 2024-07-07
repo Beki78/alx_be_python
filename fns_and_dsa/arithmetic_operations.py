f4c4cfa601000e77766a43a379c4d829e58ca30d
Baki78 <bakiget78@gmail.com>
2024-07-07 13:34:21 +0300

    divide handeling

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,11 +1,15 @@
 def perform_operation(num1, num2, operation):
     if operation == "add":
         return num1 + num2
-    if operation == "subtract":
+    elif operation == "subtract":
         return num1 - num2
-    if operation == "divide":
-        return num1 / num2
-    if operation == "multiply":
+    elif operation == "divide":
+        if num1 == 0:
+            return "error"
+        else: 
+            return num1 / num2
+    
+    elif operation == "multiply":
         return num1 * num2
     
         
