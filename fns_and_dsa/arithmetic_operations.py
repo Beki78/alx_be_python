9f12479e5a39a6d5c9c3224c2efd52fb31f5246f
Baki78 <bakiget78@gmail.com>
2024-07-07 13:11:58 +0300

    first commit

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,11 @@
+def perform_operation(x, y, operation):
+    match operation:
+        case "add":
+            return x + y
+        case "subtract":
+            return x - y
+        case "divide":
+            return x / y
+        case "multiply":
+            return x * y
+        
