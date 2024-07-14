6edb107fbebf0d88e26b4fedcce8b032c8bf29f5
Baki78 <bakiget78@gmail.com>
2024-07-14 13:20:33 +0300

    second commit

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,11 +1,13 @@
 def safe_divide(numerator, denominator):
     try:
-        float(numerator / denominator)
+        numur = float(numerator)
+        deno = float(denominator)
+        return print(numur / deno)
     except ZeroDivisionError as e:
         print(e)
     except TypeError as e:
         print("Please enter numeric values only")
-    else:
-        return print(numerator / denominator)
+ 
+        
     
-safe_divide(1.23, 0)
+safe_divide(1.23, 3)
