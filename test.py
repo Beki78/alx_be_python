23eb004e48b34fe9f5b7f74b977f03e03be0e8ae
Baki78 <bakiget78@gmail.com>
2024-07-31 01:28:55 +0300

    test dicts

--- test.py
+++ test.py
@@ -282,8 +282,213 @@
 #             list.remove(num)
 #     print(list)
 # findOccurrence(list1, 20)
+
+
+
+# list1 = [5, 20, 15, 20, 25, 50, 20]
+# print(sorted(list1))
+# def findOccurrence(list):
+#     sortedList = sorted(list)
+#     for i in range(0, len(sortedList)):
+#         if sortedList[i] == sortedList[i + 1]:
+#             sortedList.remove(sortedList[i + 1])  
+#     print(sortedList)
+# findOccurrence(list1)
+
+
+
+#   Created by Elshad Karimov on 26/04/2020.
+#   Copyright © 2020 AppMillers. All rights reserved
+
+# Dictionary Interview Questions
+
+
+# Q-1. What will be the output of the following code snippet?
+# c=[1,2,3]
+# print(c[0,1])
+# b={1:"p",2:"j"}
+# print(b[1,2])
+# a = {(1,2):1,(2,3):2}
+# print(a[(1,2)])
+# # A. Key Error
+# # B.  1
+# # C. {(2,3):2}
+# # D. {(1,2):1}
+ 
+
+# # Q-2. What will be the output of the following code snippet?
+
+# a = {'a':1,'b':2,'c':3}
+# print (a['a','b'])
+# # # A. Key Error
+# # B. [1,2]
+# # C. {‘a’:1,’b’:2}
+# # D. (1,2)
+
+ 
+
+# # Q-3. What will be the output of the following code block?
+
+# fruit = {}
+
+# def addone(key):
+#     if key in fruit:
+#         fruit[key] =fruit[key]+1
+#     else:
+#         fruit[key] = 1
+        
+# addone('Apple')
+# addone('Banana')
+# addone('apple')
+# addone('apple')
+# addone('apple')
+# print (fruit)
+# A. 1 
+# B. 2
+# C. 3 
+# D. 4
+
+ 
+
+# Q-4. What will be the output of the following code block?
+
+# arr = {}
+# arr[1] = 1
+# arr['1'] = 2
+# arr[1] += 1
+
+# sum = 0
+# for k in arr:
+#     sum += arr[k]
+
+# print(sum)
+ # A. 1 
+ # B. 2
+ # C. 3 
+ # D. 4
+
+ 
+
+# Q-5. What will be the output of the following code snippet?
+
+
+
+# my_dict = {}
+# my_dict[1] = 1
+# my_dict['1'] = 2
+# my_dict[1.0] = 4
+
+# sum = 0
+# for k in my_dict:
+#     sum += my_dict[k]
     
-    
+# print (sum)
+# A. 7
+# B. Syntax error
+# C. 3
+# D. 6
+
+ 
+
+# # Q-6. What will be the output of the following code snippet?
+
+# my_dict = {(1,2,4): 8, }
+# my_dict[(1,2,4)] = 8
+# my_dict[(4,2,1)] = 10
+# my_dict[(1,2)] = 12
+
+# sum = 0
+# for k in my_dict:
+#     sum += my_dict[k]
+
+# print (sum)
+# print(my_dict)
+# # A. Syntax error
+# # B. 30   
+# #     {(1, 2): 12, (4, 2, 1): 10, (1, 2, 4): 8}
+# # C. 47
+# #     {(1, 2): 12, (4, 2, 1): 10, (1, 2, 4): 8}
+# # D. 30
+# #     {[1, 2]: 12, [4, 2, 1]: 10, [1, 2, 4]: 8}
+
+ 
+
+# # Q-7. What will be the output of the following code snippet?
+
+# box = {}
+# jars = {}
+# crates = {}
+# box['biscuit'] = 1
+# box['cake'] = 3
+# jars['jam'] = 4
+# crates['box'] = box
+# crates['jars'] = jars
+# print(len(crates[box]))
+
+# # A. 1
+# # B. 3
+# # C. 4
+# # D. Type Error
+
+ 
+
+# # Q-8. What will be the output of the following code block?
+
+# dict = {'c': 97, 'a': 96, 'b': 98}
+
+# for key in sorted(dict):
+#     print (dict[key])
+# A. 96 98 97
+# B. 96 97 98
+# C. 98 97 96
+# D. NameError
+
+ 
+
+# # Q-9. What will be the output of the following code snippet?
+
+# rec = {"Name" : "Python", "Age":"20"}
+# id1 = rec
+# del rec
+# # rec = {"Name" : "Python", "Age":"20"}
+
+# print(id(id1))
+# # print(id(rec))
+
+# print(id(r) == id(rec))
+# # A. True
+# # B. False
+# # C. 0
+# # D. 1
+
+ 
+
+# # Q-10. What will be the output of the following code snippet?
+
+# rec = {"Name" : "Python", "Age":"20", "Addr" : "NJ", "Country" : "USA"}
+# id1 = id(rec)
+# del rec
+# rec = {"Name" : "Python", "Age":"20", "Addr" : "NJ", "Country" : "USA"}
+# id2 = id(rec)
+# print(id1)
+# print(id2)
+# print(id1 == id2)
+# # A. True
+# # B. False
+# # C. 1
+# # D. Exception
+rec = {"Name" : "Python", "Age":"20", "Addr" : "NJ", "Country" : "USA"}
+id1 = id(rec)
+id2 = id(rec)
+print(id1 == id2)
+rec = {"Name" : "Python", "Age":"20", "Addr" : "NJ", "Country" : "USA"}
+id1 = id(rec)
+id2 = id(rec)
+print(id(id1))
+print(id(id2))
+print(id(id1) == id(id2))
+
+
 
     
 
