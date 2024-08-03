1a04df28cb1f74a23a7e2d849524c4a2c9015151
Baki78 <bakiget78@gmail.com>
2024-08-04 01:10:27 +0300

    test advancedDs

--- advancedDS.py
+++ advancedDS.py
@@ -17,3 +17,28 @@
 # singleLinkedList.head.next = node2
 # singleLinkedList.tail = node2
 
+
+
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
+# class SLL:
+#     def __init__(self):
+#         self.head = None
+#     def display(self):
+#         if self.head == None:
+#             print("Linked list is empty")
+#         else:
+#             temp = self.head
+#             while temp:
+#                 print(temp.data, "-->", end=" ")
+#                 temp = temp.next
+          
+
+# L = SLL()
+# n1 = Node(10)
+# L.head = n1
+# n2 = Node(20)
+# L.head.next = n2
+# L.display()
